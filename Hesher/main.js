a91fcdafc8c2bbbd5dad0f5da3273e29e857f6c0
HesherPrice <163502008+HesherPrice@users.noreply.github.com>
2024-03-15 12:19:07 +0600

    Add files via upload

---
+++ Hesher/main.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', () => {
+    // Получаем элемент логотипа
+    const logo = document.querySelector('.logo img');
+    
+    // Добавляем класс с анимацией
+    logo.classList.add('logo-animation');
+});
